a09fe691aa991f1a02fd4ba264cb75cf6ac518d7
it-moisesmoreno <130410927+it-moisesmoreno@users.noreply.github.com>
2023-04-12 10:56:47 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/java/fundamentals/BasicSyntax.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.fundamentals;
+
+/**
+ *
+ * @author Moisés Moreno <Moisés.Moreno at linkedin.com/in/it-moisesmoreno>
+ */
+public class BasicSyntax {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
