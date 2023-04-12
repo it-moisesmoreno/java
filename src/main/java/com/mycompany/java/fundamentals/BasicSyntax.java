cb41a1224cb34e42f7ca0e9bfc18bf9f9df5eec8
it-moisesmoreno <130410927+it-moisesmoreno@users.noreply.github.com>
2023-04-12 11:31:00 -0600

    Update BasicSyntax.java
    Added basic syntax code

--- src/main/java/com/mycompany/java/fundamentals/BasicSyntax.java
+++ src/main/java/com/mycompany/java/fundamentals/BasicSyntax.java
@@ -1,10 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.java.fundamentals;
 
 /**
+ * documentation
  *
  * @author Moisés Moreno <Moisés.Moreno at linkedin.com/in/it-moisesmoreno>
  */
@@ -12,5 +12,11 @@ public class BasicSyntax {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        /*
+			System.out.println("GFG!");
+			System.out.println("Alice!");
+         */
+        System.out.println("Alice!");
     }
 }
