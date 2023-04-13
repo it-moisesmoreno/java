85a0a547ab1d9adf4df81485269f5609efe845a0
it-moisesmoreno <130410927+it-moisesmoreno@users.noreply.github.com>
2023-04-13 11:04:53 -0600

    Abstraction
    Added Abstraction examples

---
+++ src/main/java/com/mycompany/java/fundamentals/opp/abstraction/Main.java
@@ -0,0 +1,23 @@
+package com.mycompany.java.fundamentals.opp.abstraction;
+
+//abstract class
+abstract class Car {
+
+    abstract void accelerate();
+}
+
+//concrete class
+class Suzuki extends Car {
+
+    void accelerate() {
+        System.out.println("Suzuki::accelerate");
+    }
+}
+
+class Main {
+
+    public static void main(String args[]) {
+        Car obj = new Suzuki();				//Car object =>contents of Suzuki
+        obj.accelerate();					//call the method  
+    }
+}
